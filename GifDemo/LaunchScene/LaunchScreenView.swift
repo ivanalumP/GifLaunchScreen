601e5bb3050f7c452fd697699d190ae7f8860ea1
ivanalum <ivanalum@gmail.com>
2020-06-12 23:20:10 +0800

    first commit

---
+++ GifDemo/LaunchScene/LaunchScreenView.swift
@@ -0,0 +1,30 @@
+//
+//  LaunchScreenViewController.swift
+//  GifDemo
+//
+//  Created by Ivana on 12/06/2020.
+//  Copyright © 2020 Ivana. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenView: UIView {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GifDemo/Utility/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  GifDemo
+//
+//  Created by Ivana on 12/06/2020.
+//  Copyright © 2020 Ivana. All rights reserved.
+//
+
+import Foundation
